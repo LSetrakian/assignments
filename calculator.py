14d3d14bfb3105d84297823ccc1b67ba92286827
LISA S <LISASETRAKIAN2010@GMAIL.COM>
2019-01-09 18:16:08 -0500

    add functions to calculator

---
+++ calculator.py
@@ -0,0 +1,13 @@
+# This part of the code defines a function
+def multiply(a,b):
+    return a * b
+
+def add(a,b):
+	return a + b
+
+def subtract(a,b):
+	return a - b
+
+def divide(a,b):
+	return a / b
+	
