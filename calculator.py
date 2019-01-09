8ca9d400d101504d3b51bcb0dd5132b6dca7fea3
LISA S <LISASETRAKIAN2010@GMAIL.COM>
2019-01-09 18:26:43 -0500

    added a line explaining what is happening

--- calculator.py
+++ calculator.py
@@ -10,4 +10,12 @@ def subtract(a,b):
 
 def divide(a,b):
 	return a / b
-	
+
+print("I'm going use the calculator functions to multiply 5 and 6")
+
+
+# This part of the code then calls that function and assigns the variable x to its return value
+x = multiply(5,6)
+
+# This prints x to the terminal for humans to read
+print(x)
