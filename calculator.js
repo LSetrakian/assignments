d34af1d599304cf5a3469bbf05791fac956ca873
LISA S <LISASETRAKIAN2010@GMAIL.COM>
2019-01-09 22:17:45 -0500

    add functions to js

---
+++ calculator.js
@@ -0,0 +1,22 @@
+// This part of the code defines a function
+function multiply(a,b) { 
+    return a * b
+} 
+function add(a,b) { 
+    return a + b
+}
+function subtract(a,b) { 
+    return a - b
+} 
+function divide(a,b) { 
+    return a / b
+}
+function square(a) { 
+    return a ** 2
+}
+function cube(a) { 
+    return a ** 3
+}
+function square_n_times(number,n) { 
+    return number ** n
+}
