1359b24a9ba0c214dffddce0a2b074660a08d09c
LISA S <LISASETRAKIAN2010@GMAIL.COM>
2019-01-15 17:54:07 -0500

    add read write code

---
+++ pythondata/readwrite.py
@@ -0,0 +1,10 @@
+# READ name.txt into a variable my_name
+with open('name.txt') as f:
+    my_name = f.read()
+
+# construct greeting
+greeting = 'Hello my name is ' + my_name + '.'
+
+# WRITE that to a file
+with open('hello.txt', 'w') as f:
+    f.write(greeting)
