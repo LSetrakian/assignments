f503453b593da28f3b6d1e8eae07fc5b1c81d8d4
LISA S <LISASETRAKIAN2010@GMAIL.COM>
2019-01-17 11:45:08 -0500

    add cook veggies JSON exercise

---
+++ pythondata/cookveggies.py
@@ -0,0 +1,11 @@
+# Read vetegables.csv file
+import csv
+import json
+
+with open('vegetables.csv', 'r') as f:
+    reader = csv.DictReader(f)
+    vegetables = [dict(row) for row in reader]# Convert Ordered Dict to regular dict (python 3.6 or higher)
+
+# Write vegetables.JSON file
+with open('vegetables.json', 'w') as f:
+    json.dump(vegetables, f, indent=2)
